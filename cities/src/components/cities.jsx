2474c3daf2d2d3ae9e4cd6f1e841549162b62896
rawand-hawwari <rawandh3@gmail.com>
2023-10-23 23:46:07 +0300

    update

--- cities/src/components/cities.jsx
+++ cities/src/components/cities.jsx
@@ -4,14 +4,15 @@ const CityCard = ({ city }) => {
   return (
     <div className="bg-white p-4 rounded shadow">
       <h2 className="text-lg font-semibold mb-2">{city.name}</h2>
-      <img src={city.image} alt={city.name} className="w-full h-32 object-cover mb-2" />
+      <img src={city.img} alt={city.name} className="w-full h-32 object-cover mb-2" />
       <p>{city.info}</p>
     </div>
   );
 };
 
 const Cities = ({ selectedCountry, cities }) => {
-  const filteredCities = cities.filter((city) => city.country === selectedCountry);
+  console.log(selectedCountry);
+  const filteredCities = cities.filter((city) => city.name === selectedCountry);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-8">
--- cities/src/components/hero.jsx
+++ cities/src/components/hero.jsx
@@ -1,24 +1,18 @@
 import React from 'react'
 
-const Hero = (props) => {
-  const [selectedCountry, setSelectedCountry] = useState(null);
+const Hero = ({cities,setSelectedCountry}) => {
   return (
-    <div className="hero bg-[url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F445715694348307543%2F&psig=AOvVaw12oazxTGirAn05uraTl1bN&ust=1698174625778000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIiJxMzvjIIDFQAAAAAdAAAAABAI)] w-auto">
+    <div className="hero bg-[url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F445715694348307543%2F&psig=AOvVaw12oazxTGirAn05uraTl1bN&ust=1698174625778000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIiJxMzvjIIDFQAAAAAdAAAAABAI)] w-auto flex justify-center items-center text-white text-xl">
       <div className="content">
         <h1>Welcome to a Global Journey</h1>
-        <label for="cities" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-        <select id="cities" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <label for="cities" class="block mb-2 text-sm font-medium text-white dark:text-white">Select an option</label>
+        <select onChange={(e) => setSelectedCountry(e.target.value)} id="cities" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           <option selected>Choose a country</option>
-          <option value="1">New York</option>
-          <option value="2">Paris</option>
-          <option value="3">Tokyo</option>
-          <option value="4">London</option>
-          <option value="5">Sydney</option>
-          <option value="6">Rome</option>
-          <option value="7">Rio de Janeiro</option>
-          <option value="8">Dubai</option>
-          <option value="9">Cape Town</option>
-          <option value="10">Berlin</option>
+          {cities.map((city, index) => (
+            <option key={index} value={city}>
+              {city}
+            </option>
+          ))}
         </select>
       </div>
     </div>
